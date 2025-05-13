1512fc193e05b49239a90e7f879347a0f9ac737c
Ken <charlesplays15@gmail.com>
2025-05-13 10:09:45 +0800

    totoy

--- csp-app/src/Components/Footer.jsx
+++ csp-app/src/Components/Footer.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
       <footer class="site-footer">
-        <p>&copy; 2025 Your Company Name. All rights reserved.</p>
+        <p>Developed by</p>
         <nav>
-          <a href="/privacy-policy">Privacy Policy</a> |
-          <a href="/terms-of-service">Terms of Service</a> |
-          <a href="/contact">Contact Us</a>
+          <p>Charles Kent Labrador</p>
+          <p>Ismael III Baguilar </p>
+          <p>Ian Barrientos</p>
+          <p>Gryxdane Maldo</p>
+          <p>Johnroy Chatto</p>
         </nav>
+        <Link to="/admin">
+          <p>👾</p>
+        </Link>
       </footer>
     </div>
   );
