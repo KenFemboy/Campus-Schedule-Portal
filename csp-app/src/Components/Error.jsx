eda4e45423c34b507771dd8bf056e3e47c99fa76
Ken <charlesplays15@gmail.com>
2025-05-14 00:18:53 +0800

    fixed error

--- csp-app/src/Components/Error.jsx
+++ csp-app/src/Components/Error.jsx
@@ -6,8 +6,8 @@ const Error = () => {
     <div class="error">
       <img src="/logo-csp.png" alt="csp-logo" class="logo-csp" />
       <p>SOMETHING WENT WRONG</p>
-      <Link>
-        <a>Go Back</a>
+      <Link to="/">
+        <b>Go Back</b>
       </Link>
     </div>
   );
